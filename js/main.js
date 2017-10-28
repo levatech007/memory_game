6ca296b1387f686a260c58b14be0f98e1c209577
Eva <evaliisagalen@gmail.com>
2017-10-28 22:59:28 +0300

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay);
 
-console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] ) {
+
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!");
+};
