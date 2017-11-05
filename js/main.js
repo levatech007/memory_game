5bcbe22de6bf9c9fdf219137c4d04666bf64d800
Eva <evaliisagalen@gmail.com>
2017-11-05 18:10:33 +0200

    finished js functions

--- js/main.js
+++ js/main.js
@@ -24,22 +24,34 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+
+	this.setAttribute("src", cards[cardId].cardImage);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 	} else {
 	alert("Sorry, try again.");
-	}
+	};
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
 
+	cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	console.log(cardId);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();	
-
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);	
+	};
+};
+
+createBoard();
